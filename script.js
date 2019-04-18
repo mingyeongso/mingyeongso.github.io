924a57a9130fea17e1d37f6046e98a25f084cb14
042mgs.github.io <ad.snoopso@gmail.com>
2019-04-19 00:12:23 +0900

    dd
    dd

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
-/* javascript */
+/* scroll */
 window.addEventListener('scroll', function() {
   var el = document.querySelector('.nav');
   
-  if(window.scrollY >= 680) el.classList.add('shown');
+  if(window.scrollY >= 400) el.classList.add('shown');
   else el.classList.remove('shown');
 });
 
@@ -12,4 +12,19 @@ window.addEventListener('scroll', function() {
   
   if(window.scrollY >= 100) el.classList.add('shown');
   else el.classList.remove('shown');
+});
+
+
+window.addEventListener('scroll', function() {
+  var el = document.querySelector('.project-cover');
+  
+  if(window.scrollY >= 50) el.classList.add('hide');
+  else el.classList.remove('hide');
+});
+
+window.addEventListener('scroll', function() {
+  var el = document.querySelector('.project-cover');
+  
+  if(window.scrollY >= 50) el.classList.add('show');
+  else el.classList.remove('show');
 });
