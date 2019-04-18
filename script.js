a804800b326cfc67ce21b69fab11abd42f421c9f
042mgs.github.io <ad.snoopso@gmail.com>
2019-04-19 03:48:55 +0900

    ddd

--- script.js
+++ script.js
@@ -15,16 +15,57 @@ window.addEventListener('scroll', function() {
 });
 
 
-window.addEventListener('scroll', function() {
-  var el = document.querySelector('.project-cover');
-  
-  if(window.scrollY >= 50) el.classList.add('hide');
-  else el.classList.remove('hide');
-});
+function button2_click() {
+	var chip01 = document.querySelector('.chip01');
+	var chip02 = document.querySelector('.chip02');
+	var chip03 = document.querySelector('.chip03');
+	var row01 = document.querySelector('.row01');
+	var row02 = document.querySelector('.row02');
 
-window.addEventListener('scroll', function() {
-  var el = document.querySelector('.project-cover');
-  
-  if(window.scrollY >= 50) el.classList.add('show');
-  else el.classList.remove('show');
-});
+	chip01.classList.remove('active');
+	chip01.classList.add('disabled');
+	chip02.classList.add('active');
+	chip02.classList.remove('disabled');
+	chip03.classList.remove('active');
+	chip03.classList.add('disabled');
+	row02.classList.add('hide');
+	row01.classList.remove('hide');
+
+
+}
+
+function button3_click() {
+	var chip01 = document.querySelector('.chip01');
+	var chip02 = document.querySelector('.chip02');
+	var chip03 = document.querySelector('.chip03');
+	var row01 = document.querySelector('.row01');
+	var row02 = document.querySelector('.row02');
+
+	chip01.classList.remove('active');
+	chip01.classList.add('disabled');
+	chip02.classList.remove('active');
+	chip02.classList.add('disabled');
+	chip03.classList.add('active');
+	chip03.classList.remove('disabled');
+	row01.classList.add('hide');
+	row02.classList.remove('hide');
+
+}
+
+function button1_click() {
+	var chip01 = document.querySelector('.chip01');
+	var chip02 = document.querySelector('.chip02');
+	var chip03 = document.querySelector('.chip03');
+	var row01 = document.querySelector('.row01');
+	var row02 = document.querySelector('.row02');
+
+	chip01.classList.remove('disabled');
+	chip01.classList.add('active');
+	chip02.classList.remove('active');
+	chip02.classList.add('disabled');
+	chip03.classList.remove('active');
+	chip03.classList.add('disabled');
+	row01.classList.remove('hide');
+	row02.classList.remove('hide');
+
+}
